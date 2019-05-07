8dd272b44062060d6bfec7f2d761c308cc25f7c3
liouswll <2668000581@qq.com>
2019-05-07 16:24:29 +0800

    毕业设计

---
+++ vue/src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'element-ui/lib/theme-chalk/index.css'
+import ElementUI from 'element-ui';
+import axios from 'axios'
+
+
+
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.prototype.axios=axios
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
